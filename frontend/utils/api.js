f79a93b1b2d2bfd3dcaf9bac6e1a65ecd4f2fbe3
Karrma <karrmaazan@gmail.com>
2026-03-31 20:39:38 -0700

    Fix production API base URL

--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,16 +1,14 @@
 import axios from 'axios';
 
-const useProductionApi = process.env.NEXT_PUBLIC_USE_PRODUCTION_API === 'true';
 
-const baseURL = useProductionApi
-  ? 'http://localhost:80/api/v1'
-  : 'http://localhost:5000/api/v1';
+const baseURL = `${process.env.NEXT_PUBLIC_API_URL}/api/v1`;
 
-console.log('NEXT_PUBLIC_USE_PRODUCTION_API:', process.env.NEXT_PUBLIC_USE_PRODUCTION_API);
-console.log('Axios baseURL:', baseURL);
+console.log("NEXT_PUBLIC_API_URL:", process.env.NEXT_PUBLIC_API_URL);
+console.log("Axios baseURL:", baseURL);
 
 const api = axios.create({
   baseURL,
+  withCredentials: true,
 });
 
 api.interceptors.request.use(
